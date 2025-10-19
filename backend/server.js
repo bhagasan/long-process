77495b0e16f467a746d0e594c9b320309fa20935
Bhagas Adhi Nugroho <bhagas.adhi.n@gmail.com>
2025-10-19 22:11:38 +0700

    refactor server.js

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
-import express from 'express';
-import http from 'http';
-import { Server } from 'socket.io';
-import cors from 'cors';
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const cors = require('cors');
 
 const app = express();
 app.use(cors());
